37c4fbcda7b08f5699641a12e7b986e3c396cd38
Cole_Jabara <145076190+Cjabara@users.noreply.github.com>
2024-02-23 20:41:37 -0800

    Add files via upload

---
+++ Pong.py
@@ -0,0 +1,13 @@
+import turtle
+screen = turtle.Screen()
+screen.title("Pong")
+screen.setup(800, 500)
+screen.bgcolor("black")
+paddleL = 30
+paddleW = 5
+pointsLeft = 0
+pointsRight = 0
+pongstart = 250
+t= turtle.Turtle()
+t.shape("square")
+input()
